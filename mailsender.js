3393d8c2d87aeb6b4141f20288d186ec3c886731
Akuu29 <owner.akiu0529@gmail.com>
2021-06-28 08:42:56 +0900

    CLEAN Clean mailsendr.js

---
+++ mailsender.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+const nodeMailer = require("nodemailer")
+
+const sendMail = (smtpData, mailData) => {
+
+	// SMTPサーバの情報をまとめる
+	const transporter = nodeMailer.createTransport(smtpData)
+
+	// メール送信
+	transporter.sendMail(mailData, function(error, info) {
+		console.log(
+			(error) ? error : `Email sent: ${info.responce}`
+		)
+		// if(error) {
+		// 	// console.log(process.env.MAILADDRESS)
+		// 	// console.log(process.env.PASSWORD)
+		// 	// エラー
+		// 	console.log(error);
+		// } else {
+		// 	// 送信成功
+		// 	console.log("Email sent: " + info.responce)
+		// }
+	});
+}
+
+const senderMain = () => {
+	// SMTP情報を格納(Gmailの場合)
+	const smtpData = {
+		host: "smtp.gmail.com",	// Gmailサーバ
+		port: "465", 			// Gmailの場合 SSL: 465 / TLS: 587
+		secure: true,			// true > SSL
+		auth: {
+			user: process.env.MAILADDRESS,		// メールアドレス
+			pass: process.env.PASSWORD			// パスワード
+		}
+	}
+
+	// メール内容の設定
+	const mailData = {
+		from: smtpData.auth.user,			// 送信元
+		to: process.env.MAILADDRESS,			// 送信先
+		subject: "Hello",					// 件名
+		text: "テストメール",					// 通常のメール本文
+		html: "<b>htmlテストメール</b>"			// HTMLメール
+	}
+
+	// メール送信
+	sendMail(smtpData, mailData)
+}
+
+senderMain()
