a896da25f2c0ee938662e5c3aa68e7460e1312ee
Akuu29 <owner.akiu0529@gmail.com>
2021-06-30 23:42:32 +0900

    ADD
    Set up proxy server.

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+fetch("/api/").then(responce => {
+	console.log(responce.json())
+})
+
 const Index = () => {
 	return  <div>Hello React!</div>
 }
