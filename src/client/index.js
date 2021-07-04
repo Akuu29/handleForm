30ad20fc992414546177895035ace8a995501fd9
Akuu29 <owner.akiu0529@gmail.com>
2021-07-04 22:56:37 +0900

    ADD create contact form.

--- src/client/index.js
+++ src/client/index.js
@@ -6,7 +6,37 @@ fetch("/api/").then(responce => {
 })
 
 export const Index = () => {
-	return  <div>Hello React!</div>
+	return (
+		<div>
+			<form method="post" action="#">
+				<div>
+					<div>Contact Form</div>
+					<dl>
+						<dt>
+							<label>Name</label>
+						</dt>
+						<dd>
+							<input type="text" mexLength="10" placeholder="First Name"/>
+							<input type="text" maxLength="10" placeholder="Last Name" />
+						</dd>
+					</dl>
+					<dl>
+						<dt>
+							<label>Other</label>
+						</dt>
+						<dd>
+							<textarea row="5" placeholder="Other" />
+						</dd>
+					</dl>
+					<div>
+						<button type="submit">
+							SENT
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	)
 }
 
 ReactDOM.render(<Index />, document.getElementById("index"))
