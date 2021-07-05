f5b11ab314ec8d3d01e27dd5aedd11c48c00b609
Akuu29 <owner.akiu0529@gmail.com>
2021-07-05 23:44:00 +0900

    ADD Install body-parser.

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import path from "path"
+import bodyParser from "body-parser"
 
 const app = express()
 
@@ -9,8 +10,17 @@ app.get("/api", (req, res) => {
 	req.send({api: "text"})
 })
 
-app.get("*", function(req, res) {
+app.get("*", (req, res) => {
 	res.sendFile(path.resolve("./", "dist", "index.html"))
+})
+
+app.post("/post", (req, res) => {
+	console.log("success")
+	// const contactInfo = req.body
+
+	// console.log(contactInfo)
+	// const firstName = contactInfo.firstName
+	// const lastName = conactInfo.lastName
 })
 
 app.listen(3000, () => {
